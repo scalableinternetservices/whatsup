b352363f49307819ced4591851d08aa63fceac83
Alexander Yang <alexanderyang@wifi-131-179-50-36.host.ucla.edu>
2015-04-15 11:21:23 -0700

    added redirection and resource files

--- whatsup/app/controllers/users_controller.rb
+++ whatsup/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.new(article_params)
+  end
+
+  private
+    def article_params
+      params.require(:user).permit(:user, :email, :password, :password_confirmation)
+    end
 end
