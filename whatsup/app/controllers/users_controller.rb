52a56120228101dd14298b9685db94d573eee8b7
Alexander Yang <alexanderyang@Alexanders-MacBook-Pro.local>
2015-04-16 00:23:10 -0700

    Added error messages to user sign up

--- whatsup/app/controllers/users_controller.rb
+++ whatsup/app/controllers/users_controller.rb
@@ -4,10 +4,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(article_params)
+    if @user.save
+      #
+    else
+      render :template => 'auth/signup'
+    end
   end
 
   private
     def article_params
-      params.require(:user).permit(:user, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
