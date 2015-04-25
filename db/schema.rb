a47e65e407b43ba3544a0eef1ff6f1aaccc84526
Yuguang chen <ychen519@g.ucla.edu>
2015-04-24 17:27:25 -0700

    location feature sample

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414210408) do
+ActiveRecord::Schema.define(version: 20150424171401) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
