092bdf9f02f425ac12dfef695f2611378cf7f545
Alexander Yang <alexanderyang@wifi-131-179-50-194.host.ucla.edu>
2015-04-28 13:11:09 -0700

    webpages will redirect to users/new if no one is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include UsersHelper
+
+  before_action :check_log_in
+
+  private
+  
+  def check_log_in
+    if !logged_in?
+      redirect_to action: 'new', controller: 'users'
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  skip_before_action :check_log_in, only: [:new, :create, :login]
+
   def new
     if logged_in?
       @user = current_user
