1f99b22dee282fff42f209cc8b7aeed987ee491c
Alexander Yang <alexanderyang@Alexanders-MacBook-Pro.local>
2015-04-30 19:55:33 -0700

    fixed migrations, hopefully will now work on aws

--- db/migrate/20150430022101_fix_column_name.rb
+++ db/migrate/20150430022101_fix_column_name.rb
@@ -1,6 +1,6 @@
 class FixColumnName < ActiveRecord::Migration
   def change
-    rename_column :Attendances, :user, :user_id
-    rename_column :Attendances, :event, :event_id
+    rename_column :attendances, :user, :user_id
+    rename_column :attendances, :event, :event_id
   end
 end
