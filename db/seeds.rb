603a5a2d28e60d5ce7878356a4fa6634c686542b
Jennifer Huang <jennhuang8@gmail.com>
2015-05-02 16:52:49 -0700

    seed data; to use: rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+	name: 'Jane Doe',
+	email: 'test@gmail.com',
+	password: '123456',
+	password_confirmation: '123456'
+	)
+
+Event.create(
+	name: 'learn ruby',
+	location: 'boelter',
+	start_time: Time.now,
+	end_time: Time.now,
+  	description:
+    %{  Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox. }
+  )
+
+Event.create(
+	name: 'Go to Disneyland',
+	location: 'Disneyland',
+	start_time: Time.now,
+	end_time: Time.now, 
+  	description:
+    %{The Hill is going to Disneyland! }
+  )
