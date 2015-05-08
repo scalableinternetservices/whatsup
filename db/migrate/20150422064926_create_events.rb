84af239c8ca55516b78f926e541244b0883b3a95
Alexander Yang <alexanderyang@wifi-131-179-50-51.host.ucla.edu>
2015-05-08 16:52:17 -0700

    changed tables to have 20, 10 precision

--- db/migrate/20150422064926_create_events.rb
+++ db/migrate/20150422064926_create_events.rb
@@ -3,8 +3,8 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :name
       t.string :city
-      t.decimal :latitude,  :precision => 10, :scale => 3
-      t.decimal :longitude, :precision => 10, :scale => 3
+      t.decimal :latitude,  :precision => 20, :scale => 10
+      t.decimal :longitude, :precision => 20, :scale => 10
       t.timestamp :time
       t.text :description
 
