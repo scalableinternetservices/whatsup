7e05d105caf4cf8c477c1ff42d397ab4df221d96
Venudhar Ravishankar <venu.ravishankar@gmail.com>
2015-05-12 21:44:22 -0700

    Disallow user from making event ending before event beginning; maybe fixed problem with Google maps sometimes not loading fully

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,13 @@ class Event < ActiveRecord::Base
   geocoded_by :location
   after_validation :geocode
   validates :name, presence: true
-  validates_datetime :end_time, :between => [:start_time, :now]
+  #validates_datetime :end_time, :after => [:now, :start_time]
+  validate :validate_end_after_start
+  
+  def validate_end_after_start
+    if (self.end_time < self.start_time)
+      errors.add(:end_time, "must be after the start time.") 
+      return false
+    end
+  end
 end
